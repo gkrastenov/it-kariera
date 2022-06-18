3f34715dcfc751046f6728b6716567e4cf905265
Georgi Krastenov <gkrastenov@gmail.com>
2022-06-18 15:51:23 +0300

    Add second exam for introduction to Algorithms and Data Structures

---
+++ 04. Introduction-to-Algorithms-And-Data-Structures/08. Preparation-for-Exam/Exam2/Task/Movie.cs
@@ -0,0 +1,22 @@
+private string title;
+private double rating;
+
+public Movie(string title, double rating)
+{
+  //TODO: Добавете вашия код тук …                     
+}
+
+public string Title
+{
+  //TODO: Добавете вашия код тук …                   
+}
+
+public double Rating
+{
+  //TODO: Добавете вашия код тук …                      
+}
+
+public override string ToString()
+{
+  //TODO: Добавете вашия код тук …                   
+}
---
+++ 04. Introduction-to-Algorithms-And-Data-Structures/08. Preparation-for-Exam/Exam2/Task/VideoLibrary.cs
@@ -0,0 +1,52 @@
+private string name;
+private List<Movie> movies;
+
+public VideoLibrary (string name)
+{
+  //TODO: Добавете вашия код тук …                           
+} 
+
+public string Name
+{
+  //TODO: Добавете вашия код тук …                             
+}
+
+public List<Movie> Movies
+{
+  //TODO: Добавете вашия код тук …                   
+}
+
+public void AddMovie(string title, double rating)
+{
+  //TODO: Добавете вашия код тук …                   
+}
+
+public double AverageRating()
+{
+  //TODO: Добавете вашия код тук …                              
+}
+
+public List<string> GetMoviesByRating(double rating)
+{
+  //TODO: Добавете вашия код тук …                             
+}
+
+public List<Movie> SortByTitle()
+{
+  //TODO: Добавете вашия код тук …                             
+}
+
+public List<Movie> SortByRating()
+{
+  //TODO: Добавете вашия код тук …                             
+}
+
+public string[] ProvideInformationAboutAllMovies()
+{
+  //TODO: Добавете вашия код тук …                            
+}
+
+public bool CheckMovieIsInVideoLibrary (string title)
+{
+  //TODO: Добавете вашия код тук …                   
+}
